6f6889d418503bfa7a99ae160a041fa0eb9a1615
Валерий Таракановский <valery.tarakanovskiy@gmail.com>
2024-09-13 14:58:22 +0800

    Added dialog branches' instances

--- src/dialog-branches/environment.lisp
+++ src/dialog-branches/environment.lisp
@@ -16,6 +16,7 @@
 (defmethod on-callback-button ((callback callback-button-input))
 	(option-action-by-data update:callback-data))
 
+
 #| Branch Initial |#
 
 (defclass branch-initial (dialog-branch) ())
@@ -67,7 +68,7 @@
 	  ((:text "Назад" 
 	    :action (:back))
 	   (:text "Отправить данные" 
-	    :action (:next-branch 'branch-interact-finish)))))
+	    :action ((:next-branch 'branch-interact-finish))))))
 
 
 #| Branch People Data |#
@@ -89,27 +90,120 @@
 	  ((:text "Назад"
 	   	:action (:back))
 	   (:text "Добавить"
-	    :action ((:next-branch 'person-data-last-name))))))
+	    :action ((:next-branch 'branch-person-data-last-name))))))
 
 
-#| Person Data Last Name |#
+#| Branch Person Data Last Name |#
 
-(defclass branch-person-data-last-name (branch-text-input))
+(defclass branch-person-data-last-name (branch-text-input) ())
 
 (defmethod branch-message ((branch branch-person-data-last-name))
 	(format nil "Введите фамилию:"))
 
-(defmethod reply-markup-type ((branch branch-people-data))
+(defmethod reply-markup-type ((branch branch-person-data-last-name))
 	(make-instance 'inline-markup))
 
 (defmethod reply-options ((branch branch-person-data-last-name))
 	'(((:text "Отменить ввод данных"
 		:action (:back)))))
 
-(defmethod read-user-message ((branch person-data-last-name))
+(defmethod read-user-message ((branch branch-person-data-last-name))
 	(case 
 		((string= (update:message-text) "")
 			(api:text-back "Необходимо ввести фамилию!"))
 		(t 
 			(let ((person-data-temp (mito:find-dao 'person-data-temp 
-											:user-id (slot-value update:*user-db* 'id))))))))
+									 :user-id (slot-value update:*user-db* 'id))))
+				(setf (slot-value person-data-temp 'last-name) (update:message-text))
+				(mito:save-dao person-data-temp))
+			(run-action '(:next-branch 'branch-person-data-first-name)))))
+
+
+#| Branch Person Data First Name |#
+
+(defclass branch-person-data-first-name (branch-text-input) ())
+
+(defmethod branch-message ((branch branch-person-data-first-name))
+	(format nil "Введите имя:"))
+
+(defmethod reply-markup-type ((branch branch-person-data-first-name))
+	(make-instance 'inline-markup))
+
+(defmethod reply-options ((branch branch-person-data-first-name))
+	'(((:text "Назад"
+		:action (:back)))
+	  ((:text "Отменить ввод данных"
+	  	:action (:cancel)))))
+
+(defmethod read-user-message ((branch branch-person-data-first-name))
+	(case
+		((string= (update:message-text) "")
+			(api:text-back "Необходимо ввести имя!"))
+		(t
+			(let ((person-data-temp (mito:find-dao 'person-data-temp
+									 :user-id (slot-value update:*user-db* 'id))))
+				(setf (slot-value person-data-temp 'first-name) (update:message-text))
+				(mito:save-dao person-data-temp))
+			(run-action '(:next-branch 'branch-person-data-middle-name)))))
+
+
+#| Branch Person Data Middle Name |#
+
+(defclass branch-person-data-middle-name (branch-text-input) ())
+
+(defmethod branch-message ((branch branch-person-data-middle-name))
+	(format nil "Введите отчество:"))
+
+(defmethod reply-markup-type ((branch branch-person-data-middle-name))
+	(make-instance 'inline-markup))
+
+(defmethod reply-options ((branch branch-person-data-middle-name))
+	'(((:text "Назад"
+		:action (:back)))
+	  ((:text "Отменить ввод данных"
+	  	:action (:cancel)))))
+
+(defmethod read-user-message ((branch branch-person-data-middle-name))
+	(let ((person-data-temp (mito:find-dao 'person-data-temp
+									 :user-id (slot-value update:*user-db* 'id))))
+				(setf (slot-value person-data-temp 'first-name) (update:message-text))
+				(mito:save-dao person-data-temp))
+	(run-action '(:next-branch 'branch-person-data-dob)))
+
+
+#| Branch Person Data DOB |#
+
+(defclass branch-person-data-dob (dialog-branch) ())
+
+(defmethod branch-message ((branch branch-person-data-dob))
+	(format nil "Введите дату рождения:"))
+
+(defmethod reply-markup-type ((branch branch-person-data-dob))
+	(make-instance 'inline-markup))
+
+(defmethod reply-options ((branch branch-person-data-dob))
+	'(((:text "Назад"))))
+
+
+#| Branch Interact Finish |#
+
+(defclass branch-interact-finish (dialog-branch) ())
+
+(defmethod branch-message ((branch branch-interact-finish))
+	(format nil ""))
+
+(defmethod reply-options ((branch branch-interact-finish))
+	'(((:text "В начало"
+		:action ((:next-branch 'branch-initial))))))
+
+
+#| Branch Generate Finish |#
+
+(defclass branch-generate-finish (dialog-branch) ())
+
+(defmethod branch-message ((branch branch-generate-finish))
+	(format nil ""))
+
+(defmethod reply-options ((branch branch-generate-finish))
+	'(((:text "В начало"
+		:action ((:next-branch 'branch-initial))))))
